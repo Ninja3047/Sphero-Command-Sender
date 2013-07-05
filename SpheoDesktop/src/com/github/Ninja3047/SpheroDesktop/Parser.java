04b31798a2aea461516a4044c46928d163d88d6b
william <william@wtan-PC>
2013-07-05 10:17:04 -0400

    first commit

---
+++ SpheoDesktop/src/com/github/Ninja3047/SpheroDesktop/Parser.java
@@ -0,0 +1,68 @@
+package com.github.Ninja3047.SpheroDesktop;
+
+import se.nicklasgavelin.bluetooth.Bluetooth;
+import se.nicklasgavelin.bluetooth.BluetoothDevice;
+import se.nicklasgavelin.sphero.command.*;
+import se.nicklasgavelin.sphero.Robot;
+import se.nicklasgavelin.sphero.exception.InvalidRobotAddressException;
+import se.nicklasgavelin.sphero.exception.RobotBluetoothException;
+
+public class Parser
+{
+	Robot robot;
+
+	public Parser(Robot r)
+	{
+		this.robot = r;
+	}
+	
+	public void parse(String command)
+	{
+		String[] commands = command.split("\\s+");
+		
+		/*for(int i = 0; i < commands.length; i++)
+		{
+			System.out.println(commands[i]);
+		}*/
+		
+		switch(commands[0].toLowerCase())
+		{
+			case "color":
+				try
+				{
+					robot.sendCommand(new RGBLEDCommand(Integer.parseInt(commands[1]), Integer.parseInt(commands[2]), Integer.parseInt(commands[3])));
+				} catch(NumberFormatException e)
+				{
+					System.err.println("Invalid Arguments");
+					System.out.println("Syntax: color [red] [green] [blue]");
+					System.out.println("Where the colors are numbers from 0-256");
+					System.out.println("Example: color 256 0 0");
+				} catch(ArrayIndexOutOfBoundsException f)
+				{
+					System.err.println("Invalid Arguments");
+					System.out.println("Syntax: color [red] [green] [blue]");
+					System.out.println("Where the colors are numbers from 0-256");
+					System.out.println("Example: color 256 0 0");
+				}
+				break;
+			case "colour":
+				try
+				{
+					robot.sendCommand(new RGBLEDCommand(Integer.parseInt(commands[1]), Integer.parseInt(commands[2]), Integer.parseInt(commands[3])));
+				} catch(NumberFormatException e)
+				{
+					System.err.println("Invalid Arguments");
+					System.out.println("Syntax: colour [red] [green] [blue]");
+					System.out.println("Where the colours are numbers from 0-256");
+					System.out.println("Example: colour 256 0 0");
+				} catch(ArrayIndexOutOfBoundsException f)
+				{
+					System.out.println("Syntax: colour [red] [green] [blue]");
+					System.out.println("Where the colours are numbers from 0-256");
+					System.out.println("Example: colour 256 0 0");
+					System.err.println("Invalid Arguments");
+				}
+				break;
+		}
+	}
+}
---
+++ SpheoDesktop/src/com/github/Ninja3047/SpheroDesktop/SpheroDesktop.java
@@ -0,0 +1,53 @@
+package com.github.Ninja3047.SpheroDesktop;
+
+import java.util.Scanner;
+
+import se.nicklasgavelin.bluetooth.Bluetooth;
+import se.nicklasgavelin.bluetooth.BluetoothDevice;
+import se.nicklasgavelin.sphero.Robot;
+import se.nicklasgavelin.sphero.exception.InvalidRobotAddressException;
+import se.nicklasgavelin.sphero.exception.RobotBluetoothException;
+
+public class SpheroDesktop
+{
+
+	public static void main(String[] args)
+	{
+		Scanner keyboard = new Scanner(System.in);
+		String input;
+		
+		System.out.println("Welcome to Sphero Desktop Command Sender");
+		System.out.println("Version 1.0a");
+		System.out.println("By William Tan");
+		
+		System.out.println();
+		
+		System.out.print("Enter Bluetooth Id: ");
+		String bid = keyboard.nextLine();
+		
+		Bluetooth bt = new Bluetooth( Bluetooth.SERIAL_COM );
+		BluetoothDevice btd = new BluetoothDevice(bt, "btspp://" + bid + ":1;authenticate=true;encrypt=false;master=false");
+
+		try
+		{
+			Robot r = new Robot( btd );
+		    Parser parser = new Parser(r);
+			do
+			{
+				input = keyboard.nextLine();
+				parser.parse(input);
+					
+			} while(input != "exit");
+		}
+		catch (InvalidRobotAddressException e)
+		{
+			System.err.println("Invalid Robot Address Exception!");
+		}
+		catch (RobotBluetoothException e)
+		{
+			System.err.println("Robot Bluetooth Exception");
+		}
+		keyboard.close();
+	}
+}
+ 
